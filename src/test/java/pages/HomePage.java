346e36aa2f56c6c55c748c89bff5b76d91345a59
unknown <you@example.com>
2023-02-16 19:03:10 +0600

    updating classes

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import setup.SetupDriver;
 
 import java.time.Duration;
 import java.util.List;
